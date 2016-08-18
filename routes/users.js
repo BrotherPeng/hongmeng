2ec7a54a97159a858fe45f28662fe39fa7534340
luming <luming@iqianjin.com>
2016-08-18 16:50:30 +0800

    添加项目控制页面等

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var users = require('../model/users');
+var router = express.Router();
+
+module.exports=function (app,passport) {
+    /* GET users listing. */
+    app.get('/login', function(req, res, next) {
+        res.render('users/login', { title: '登录' });
+    });
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+            successRedirect: '/', // redirect to the secure profile section
+            failureRedirect: '/login', // redirect back to the signup page if there is an error
+            failureFlash: true // allow flash messages
+        }),
+        function (req, res) {
+            if (req.body.remember) {
+                req.session.cookie.maxAge = 1000 * 60 * 3;
+            } else {
+                req.session.cookie.expires = false;
+            }
+            res.redirect('/');
+        });
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
+}
+
