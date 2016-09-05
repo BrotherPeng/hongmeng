0c59d63b44369c1c77cdcb1e44b638d41432f468
luming <luming@iqianjin.com>
2016-09-05 20:45:03 +0800

    从页面下发数据

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -55,20 +55,20 @@ require(['jquery','handlebars','dialog','flatpickr'], function ($,handlebars,dia
             $content='<div class="input-group"><label>id:</label><span>'+id+'</span></div>' +
                 '<label class="radio-inline"><input type="radio" name="optionsRadios" id="optionWeek" value="0" checked>周模式</label>' +
                 '<label class="radio-inline"><input type="radio" name="optionsRadios" id="optionDay" value="1">日模式</label>' +
-                '<div class="input-group"><label>周一:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="09:30">' +
-                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="19:30"></div>' +
-                '<div class="input-group"><label>周二:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="09:30">' +
-                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="19:30"></div>' +
-                '<div class="input-group"><label>周三:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="09:30">' +
-                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="19:30"></div>' +
-                '<div class="input-group"><label>周四:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="09:30">' +
-                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="19:30"></div>' +
-                '<div class="input-group"><label>周五:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="09:30">' +
-                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="19:30"></div>' +
-                '<div class="input-group"><label>周六:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="09:30">' +
-                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="19:30"></div>' +
-                '<div class="input-group"><label>周日:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="09:30">' +
-                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="19:30"></div>' +
+                '<div class="input-group"><label>周一:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="19:30">' +
+                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="06:30"></div>' +
+                '<div class="input-group"><label>周二:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="19:30">' +
+                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="06:30"></div>' +
+                '<div class="input-group"><label>周三:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="19:30">' +
+                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="06:30"></div>' +
+                '<div class="input-group"><label>周四:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="19:30">' +
+                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="06:30"></div>' +
+                '<div class="input-group"><label>周五:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="19:30">' +
+                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="06:30"></div>' +
+                '<div class="input-group"><label>周六:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="19:30">' +
+                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="06:30"></div>' +
+                '<div class="input-group"><label>周日:</label><div class="row"><label class="col-sm-2">开启时间:</label><input class="col-sm-3 openTime flatpickr" value="19:30">' +
+                '<label class="col-sm-2">关闭时间:</label><input class="col-sm-3 closeTime flatpickr" value="06:30"></div>' +
                 '<div class="input-group"></div>';
 
         var d = dialog({
@@ -76,12 +76,15 @@ require(['jquery','handlebars','dialog','flatpickr'], function ($,handlebars,dia
             content: $content,
             okValue: '确 定',
             ok: function () {
-                var type=$('[type=radio]:checked').val();
+                var type=$('[type=radio]:checked').val(),timeArr=getTime();
+                openTime=timeArr.openTime.toString();
+                closeTime=timeArr.closeTime.toString();
+                $('.flatpickr-wrapper').remove();
                 $.ajax({
                     method: "POST",
                     url: "/nodeControl/id/"+id,
                     dataType: "json",
-                    data:{id:id,type:type},
+                    data:{id:id,type:type,openTime:openTime,closeTime:closeTime},
                     success: function (data) {
                         return false;
                     }
@@ -100,5 +103,26 @@ require(['jquery','handlebars','dialog','flatpickr'], function ($,handlebars,dia
         });
 
         d.show();
-    })
+    });
+    function getTime() {
+        var timeArr={};
+        timeArr['openTime']=[];
+        timeArr['closeTime']=[];
+        $.each($('.flatpickr-wrapper'),function (i,v) {
+            var apm=$(v).find('.flatpickr-am-pm').html(),
+                h=$(v).find('.flatpickr-hour').val(),
+                m=$(v).find('.flatpickr-minute').val();
+            if(apm==='PM'){
+                h=12+Number(h);
+            }
+            if(i%2){
+                timeArr['closeTime'].push(h);
+                timeArr['closeTime'].push(m);
+            }else{
+                timeArr['openTime'].push(h);
+                timeArr['openTime'].push(m);
+            }
+        });
+        return timeArr;
+    }
 });
