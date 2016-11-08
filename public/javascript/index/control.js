45835a13dec5dd4bd8f632284b33638d6d049edf
luming <luming@iqianjin.com>
2016-11-08 17:56:40 +0800

    首页20s自动更新数据

--- public/javascript/index/control.js
+++ public/javascript/index/control.js
@@ -24,6 +24,61 @@ require(['jquery', 'handlebars', 'dialog', 'flatpickr','dialogTemplate'], functi
     var switchPanel =dialogTemplate.switchPanel,
         weekPanel = dialogTemplate.weekPanel,
         dailyPanel = dialogTemplate.dailyPanel;
+    var AutoRefresh=function () {
+        this.autoArr=[];
+        this.autoPanel={};
+        this.run();
+    };
+    AutoRefresh.prototype={
+        set:function (id,panel) {
+            var self=this;
+            self.autoArr.push(id);
+            self.autoPanel[id]=panel;
+        },
+        get:function (id) {
+            var self=this,
+                has=false;
+            $.each(self.autoArr,function (i,v) {
+                if(v==id){
+                    has=true;
+                    return;
+                }
+            });
+            return has;
+        },
+        remove:function (id) {
+            var self=this;
+            $.each(self.autoArr,function (i,v) {
+                if(v==id){
+                    self.autoArr.splice(i,1);
+                    self.autoPanel[id]=null;
+                }
+            })
+        },
+        run:function () {
+            var self=this;
+            $.each(self.autoArr,function (i,v) {
+                self.render(v);
+            });
+            setTimeout(function () {
+                self.run();
+            },20000)
+        },
+        render:function (id) {
+            var self=this;
+            $.ajax({
+                method: "GET",
+                url: "/nodeControl/id",
+                dataType: "json",
+                data: {projectId: id},
+                success: function (data) {
+                    var result = template(data);
+                    self.autoPanel[id].html(result);
+                }
+            });
+        }
+    };
+    var autoRefresh = new AutoRefresh();
     //下拉按钮单击事件，请求该项目下的所有设备
     $('body').on('click', '#projectId .project-up-down', function () {
         var $this = $(this),
@@ -35,10 +90,12 @@ require(['jquery', 'handlebars', 'dialog', 'flatpickr','dialogTemplate'], functi
 
         if (isShow) {
             $tabpanel.addClass('fn-hide');
-            $this.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down')
+            $this.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+            autoRefresh.remove(id);
+            return;
         } else {
             $tabpanel.removeClass('fn-hide');
-            $this.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up')
+            $this.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
         }
 
 
@@ -50,6 +107,7 @@ require(['jquery', 'handlebars', 'dialog', 'flatpickr','dialogTemplate'], functi
             success: function (data) {
                 var result = template(data);
                 $statusWrap.html(result);
+                autoRefresh.set(id,$statusWrap);
             }
         });
     });
@@ -311,4 +369,6 @@ require(['jquery', 'handlebars', 'dialog', 'flatpickr','dialogTemplate'], functi
             }
         });
     }
+
+
 });
