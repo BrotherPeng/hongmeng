aaec9b8a314e9672d6032ffc366fa612656b71ff
luming <luming@iqianjin.com>
2016-11-09 19:38:28 +0800

    左上角对应页面

--- public/javascript/common/base.js
+++ public/javascript/common/base.js
@@ -15,21 +15,29 @@ $.ajax({
 getPath(location.pathname);
 function getPath(path) {
     switch (path) {
+        case '/login':
+            $('[role=navigation] .navbar-brand').html('登录');
+            break;
         case '/monitor':
+            $('[role=navigation] .navbar-brand').html('监测中心');
             $('.treeview').addClass('active');
             $('.treeview').find('.treeview-menu').show();
             break;
         case '/control':
+            $('[role=navigation] .navbar-brand').html('控制中心');
             $('.treeview').addClass('active');
             $('.treeview').find('.treeview-menu').show();
             break;
         case '/member/list':
+            $('[role=navigation] .navbar-brand').html('人员管理');
             $('.member').addClass('active');
             break;
         case '/project/list':
+            $('[role=navigation] .navbar-brand').html('项目管理');
             $('.project').addClass('active');
             break;
         case '/equipment/list':
+            $('[role=navigation] .navbar-brand').html('设备管理');
             $('.equipment').addClass('active');
             break;
         default:
