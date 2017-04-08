3293b8eecf5c74c8b91e47797ccb786cb0c6063d
yunpengl <yunpengl@zhiyoua.com>
2017-04-08 14:53:20 +0800

    数据库密码错误

--- module/connection.js
+++ module/connection.js
@@ -11,7 +11,8 @@ function Connection() {
             connectionLimit: 10,
             host: 'localhost',
             user: 'root',
-            password: 'root',
+            password: '123456',
+            // password: 'root',
             database: 'relation'
         });
     };
